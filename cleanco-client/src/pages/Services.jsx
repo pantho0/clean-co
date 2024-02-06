c7e5a9512c014cdc8164dbf2c7ab1148ba30bffc
pantho0 <panthomashreky6@gmail.com>
2024-02-06 18:27:05 +0600

    pagination added to client site

--- cleanco-client/src/pages/Services.jsx
+++ cleanco-client/src/pages/Services.jsx
@@ -5,26 +5,48 @@ import { useState } from "react";
 
 const Services = () => {
   const axiosPublic = useAxios();
-  const [slectedCat, setSelectedCat] = useState('')
-  const [price, setPrice] = useState('')
+  const [slectedCat, setSelectedCat] = useState("");
+  const [price, setPrice] = useState("");
+  const [page, setPage]=useState(1)
+
   
-  const { data: services=[], isPending } = useQuery({
-    queryKey: ["services", slectedCat, price],
+
+  const { data: services = [], isPending } = useQuery({
+    queryKey: ["services", slectedCat, price, page],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/services?category=${slectedCat}&sortField=price&sortOrder=${price}`);
-      return res.data.result;
+      const res = await axiosPublic.get(
+        `/services?category=${slectedCat}&sortField=price&sortOrder=${price}&page=${page}&limit=${limit}`
+      );
+      return res.data;
     },
-    
   });
+  const limit = 12;
+  const totalPage = Math.ceil(services.total/limit);
 
-  const category = ['Residential', 'Carpet Care', 'Commercial', 'Specialty', 'Transition', 'Environmentally Friendly', 'Organization', 'Seasonal', 'Exterior', 'Pool Services', 'Special Occasions', 'Indoor Air Quality', 'Bedroom Care', 'Emergency Services']
+  const category = [
+    "Residential",
+    "Carpet Care",
+    "Commercial",
+    "Specialty",
+    "Transition",
+    "Environmentally Friendly",
+    "Organization",
+    "Seasonal",
+    "Exterior",
+    "Pool Services",
+    "Special Occasions",
+    "Indoor Air Quality",
+    "Bedroom Care",
+    "Emergency Services",
+  ];
 
-const selectCat= (e) =>{
-    setSelectedCat(e.target.value)
-}
-const sortByPrice = (e)=>{
-    setPrice(e.target.value)
-}
+  const selectCat = (e) => {
+    setSelectedCat(e.target.value);
+  };
+  const sortByPrice = (e) => {
+    setPrice(e.target.value);
+  };
+
 
   return (
     <div>
@@ -33,34 +55,58 @@ const sortByPrice = (e)=>{
       </h2>
       <div className="flex justify-end gap-4">
         <div>
-            <p className="mb-3 text-primary font-bold">Filter By Category </p>
-          <select onChange={selectCat} className="select select-primary w-full max-w-xs">
-            <option value={''} >
-              All   
-            </option>
-            {
-                category.map(cat=><option key={cat} value={cat}>{cat}</option>)
-            }
+          <p className="mb-3 text-primary font-bold">Filter By Category </p>
+          <select
+            onChange={selectCat}
+            className="select select-primary w-full max-w-xs"
+          >
+            <option value={""}>All</option>
+            {category.map((cat) => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
           </select>
         </div>
         <div>
-        <p className="mb-3 text-primary font-bold">Filter By Price </p>
-          <select onChange={sortByPrice} className="select select-primary w-full max-w-xs">
-            <option disabled selected>
-             Select One 
+          <p className="mb-3 text-primary font-bold">Filter By Price </p>
+          <select
+            onChange={sortByPrice}
+            className="select select-primary w-full max-w-xs"
+          >
+            <option disabled >
+              Select One
             </option>
-            <option value={'asc'}>From Low To High</option>
-            <option value={'desc'}>From High To Low</option>
+            <option value={"asc"}>From Low To High</option>
+            <option value={"desc"}>From High To Low</option>
           </select>
         </div>
       </div>
-      {
-        isPending ? <div className="flex justify-center items-center h-full"><span className="loading loading-dots loading-lg bg-primary"></span></div>  : <div className="grid grid-cols-3 justify-evenly gap-10 mt-10">
-        {services.map((service) => (
-          <ServiceCard key={service._id} service={service} />
-        ))}
-      </div>
-      }
+      {isPending ? (
+        <div className="flex justify-center items-center h-full">
+          <span className="loading loading-dots loading-lg bg-primary"></span>
+        </div>
+      ) : (
+        <div>
+          <div className="grid grid-cols-3 justify-evenly gap-10 mt-10">
+          {services?.result.map((service) => (
+            <ServiceCard key={service._id} service={service} />
+          ))}
+          
+        </div>  
+        <div className="flex justify-center w-full mt-5">
+            <div className="join">
+              {[...Array(totalPage).fill(0)].map((button, idx)=><button key={idx} onClick={()=>setPage(idx+1)}
+                className={`${idx+1 === page ? 'join-item btn btn-primary' : 'join-item btn btn-square'}`}
+                type="radio"
+                name="options"
+              >{idx+1}</button>)}
+            </div>
+          </div>
+        </div>
+        
+        
+      )}
     </div>
   );
 };
